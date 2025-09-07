5a7675b77c9e6c95913ac5cc6da1a6c4025488d0
tyshchukRoman <roman.t@codelibry.dev>
2025-09-07 23:43:15 +0300

    feat: add wc gallery supports

--- inc/theme-hooks/theme-supports.php
+++ inc/theme-hooks/theme-supports.php
@@ -6,20 +6,16 @@
 function cube_supports(){
 	load_theme_textdomain( 'cube-wp', get_template_directory() . '/languages' );
 
+  // WordPress Support
 	add_theme_support( 'title-tag' );
 	add_theme_support( 'post-thumbnails' );
 	add_theme_support( 'menus' );
-  add_theme_support( 'woocommerce' );
 
-  register_nav_menus(
-    array(
-      'header-menu' => __('Header Menu', 'cube-wp'),
-      'mobile-menu' => __('Mobile Menu', 'cube-wp'),
-      'footer-menu-1' => __('Footer Col 1', 'cube-wp'),
-      'footer-menu-2' => __('Footer Col 2', 'cube-wp'),
-      'footer-menu-3' => __('Footer Col 3', 'cube-wp'),
-    )
-  );
+  // Woocommerce Support
+  add_theme_support( 'woocommerce' );
+  add_theme_support( 'wc-product-gallery-zoom' );
+  add_theme_support( 'wc-product-gallery-lightbox' );
+  add_theme_support( 'wc-product-gallery-slider' );
 }
 
 add_action( 'after_setup_theme', 'cube_supports' );
